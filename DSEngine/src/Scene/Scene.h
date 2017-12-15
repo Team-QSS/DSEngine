5f61d29bacb931b8c2ff1fe3ec39e8e180b89cb8
Splash <kkfenix3085@naver.com>
2017-12-15 20:36:48 +0900

    컨테이너

--- DSEngine/src/Scene/Scene.h
+++ DSEngine/src/Scene/Scene.h
@@ -25,7 +25,7 @@ namespace DS
 		void update(float deltaTime);
 		void draw();
 
-		std::unordered_set<Object&> m_Objects;
+		std::unordered_set<Object&> m_Objects; //현재 오브젝트들을 담는 컨테이너
 
 		friend void Object::update(float deltaTime);
 		friend void Object::draw();
