04e0e0822ee4120e31a0a58fc48d6089ba381b5e
Denews <gkvms963@naver.com>
2017-12-22 15:19:29 +0900

    .

--- DSEngine/main.cpp
+++ DSEngine/main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 #define LOG_MIN_LEVEL 1
 #include "src\Defines.h"
-#include "src\Utils\Utils.h"
 
 using namespace std;
 
 int main()
 {
-	DS::Logger::createInstance();
-	DS::Logger::getInstance().initialize(true);
-	LOG(DS::LogLevel::Info, "Log 1");
-
-	DS::Logger::destroyInstance();
 	return 0;
 }
