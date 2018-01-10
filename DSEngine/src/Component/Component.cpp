9176781248d4309c6657178714e7fdb1dc9043ea
지동준 <gkvms963@naver.com>
2018-01-10 14:38:31 +0900

    Component 멤버함수 구현

--- DSEngine/src/Component/Component.cpp
+++ DSEngine/src/Component/Component.cpp
@@ -2,5 +2,23 @@
 
 namespace DS
 {
+	Component::Component()
+	{
 
+	}
+
+	Component::~Component()
+	{
+
+	}
+
+	void Component::update(float deltaTime)
+	{
+		onUpdate(deltaTime);
+	}
+
+	void Component::draw()
+	{
+		onDraw();
+	}
 }
