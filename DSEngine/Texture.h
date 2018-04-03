1943ca11897d0bca85b73ab2bca1a42e55a5ae81
Splash <kkfenix3085@naver.com>
2018-04-03 12:03:28 +0900

    Texture 클래스 선언

---
+++ DSEngine/Texture.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "src\Utils\Logger.h"
+
+// #include <png.h>
+// #include <glew.h>
+
+namespace DS
+{
+	class Texture final
+	{
+	public:
+		Texture(const tstring& path);
+		~Texture();
+
+		const tstring& getPath() const;
+		int32 getHeight() const;
+		int32 getWidth() const;
+
+	private:
+		uint8 ReadPNG();
+		void Load();
+
+		int32 m_Width;
+		int32 m_Height;
+
+		Texture(const Texture& yRef);
+		Texture(Texture&& yRef);
+		Texture& operator= (const Texture& yRef);
+		Texture& operator= (Texture&& yRef);
+	};
+}
--- DSEngine/src/Defines.h
+++ DSEngine/src/Defines.h
@@ -16,7 +16,7 @@ using int8 = int8_t;
 using uint64 = uint64_t;
 using uint32 = uint32_t;
 using uint16 = uint16_t;
-using uint8_t = uint8_t;
+using uint8 = uint8_t;
 
 using float32 = float;
 using float64 = double;
